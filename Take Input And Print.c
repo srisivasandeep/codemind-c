0bd2af7ae68e54c734f3193486fbfb7f8c0e22ec
srisivasandeep <80574426+srisivasandeep@users.noreply.github.com>
2021-09-25 19:07:41 +0530

    #include <stdio.h> #include <string.h> #include <math.h> #include <stdlib.h>
    int main() 
    {
    	
        char s[100];
        scanf("%[^
    ]%*c", &s);
      	
        printf("%s",s);  
        return 0;
    }

---
+++ Take Input And Print.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+	
+    char s[100];
+    scanf("%[^
+]%*c", &s);
+  	
+    printf("%s",s);  
+    return 0;
+}
